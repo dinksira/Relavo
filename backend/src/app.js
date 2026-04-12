4759ca46607379a7e0ec398a745084af8d31e1cf
Dink <dinksiraelisa@gmail.com>
2026-04-12 21:51:41 +0300

    fix: allow CORS for relavo.xyz

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(cors({
-  origin: ['https://relavo.vercel.app', 'http://localhost:5173'],
+  origin: ['https://relavo.vercel.app', 'https://relavo.xyz', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
